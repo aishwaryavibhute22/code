8c2d9f4798841505912bdda20fc896ca55dea379
aishwaryavibhute22 <140887629+aishwaryavibhute22@users.noreply.github.com>
2023-07-29 15:36:54 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,57 @@
+/*
+#include<stdio.h>
+int main()
+{
+printf("Hello");
+return 0;
+}
+*/
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+ printf("Enter the hello");
+ scanf("c%",hello);
+ 
+ return 0;
+ }
+ */
+ 
+ 
+ //sum of 2 number
+ /*
+ #include<stdio.h>
+ int main()
+ {
+  int a,b;
+  int sum ;
+  printf("Enter the a");
+  scanf("%d",&a);
+  printf("Enter the b");
+  scanf("%d",&b);
+  {
+   sum = a + b;
+   printf("%d",sum);
+   }
+   return 0;
+}
+*/
+
+#include<stdio.h>
+int main(){
+   int arr[5] = {2,3,4,5,6};
+   printf("%d",arr[4]);
+   return 0;
+   } 
+   
+   
+   
+   
+   
+   
+   
+   
+   
